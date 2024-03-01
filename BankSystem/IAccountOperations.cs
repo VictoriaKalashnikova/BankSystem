39e768a957d119b202d0ba5480339df9a18f5ac9
Anton Ovcharov <antonovcharov718@gmail.com>
2024-03-01 14:13:30 +0200

    Add Account interface

---
+++ BankSystem/IAccountOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    // Account Operations interface
+    public interface IAccountOperations
+    {
+        void CreateAccount(string? clientId);
+        void CloseAccount(string? clientId);
+        bool PerformTransaction(string? clientId, decimal amount);
+    }
+}
