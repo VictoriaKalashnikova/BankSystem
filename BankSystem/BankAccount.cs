8f376b01343d242fcf8d43c85f5ba77b0a1ec48d
Anton Ovcharov <antonovcharov718@gmail.com>
2024-03-01 14:32:05 +0200

    Required

---
+++ BankSystem/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    // Bank Account class
+    public class BankAccount : IBankAccount
+    {
+        private decimal balance;
+
+        public BankAccount(decimal initialAmount)
+        {
+            balance = initialAmount;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
