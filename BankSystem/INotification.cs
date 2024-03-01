83c8936f4c5ed4ce98742d75a5fbf17f2cbfe801
Anton Ovcharov <antonovcharov718@gmail.com>
2024-03-01 14:28:48 +0200

    Add Notification interface

---
+++ BankSystem/INotification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    // Notification interface
+    public interface INotification
+    {
+        void NotifyTransaction(string? clientId, decimal amount);
+        void NotifyBalanceChange(string? clientId, decimal newBalance);
+    }
+}
